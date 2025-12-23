782053d8512da42026cdccb1c8c62de07ac85388
jajanidzee <jajanidzekote310@gmail.com>
2025-12-23 12:21:11 +0400

    123

--- src/Components/Routeing/Routeing.jsx
+++ src/Components/Routeing/Routeing.jsx
@@ -3,21 +3,36 @@ import { Link } from "react-router-dom";
 
 function Routeing() {
   return (
-    <div className=" w-[100%] justify-between flex">
+    <div className="w-[100%] justify-between flex">
       <h1 className="Rose text-[150px] font-[East_Sea_Dokdo] hover:text-[200px] hover:text-red-500 ">
         <Link to="/"> Rose </Link>
       </h1>
 
-      <ul className="text-[34px]  gap-[4px]  flex   ">
-        <li className="inline font-[EB_Garamond] hover:text-[50px]  cursor-pointer hover:text-red-500  ">
-          <Link to="/About"> About,</Link>
+      <ul className="text-[34px] gap-[4px] flex">
+        <li className="inline   ">
+          <Link
+            to="/About"
+            className="font-[EB_Garamond] hover:text-[50px]  cursor-pointer hover:text-red-500"
+          >
+            {" "}
+            About,
+          </Link>
         </li>
 
-        <li className="font-[EB_Garamond] hover:text-[50px] cursor-pointer hover:text-red-500  ">
-          <Link to="/Experiences"> Experiences, </Link>
+        <li className=" ">
+          <Link
+            to="/Experiences"
+            className="font-[EB_Garamond] hover:text-[50px] cursor-pointer hover:text-red-500 "
+          >
+            {" "}
+            Experiences,{" "}
+          </Link>
         </li>
-        <li className="font-[EB_Garamond] hover:text-[50px] cursor-pointer hover:text-red-500  ">
-          Email
+        <li className=" ">
+          <Link className="font-[EB_Garamond] hover:text-[50px] cursor-pointer hover:text-red-500">
+            {" "}
+            Email
+          </Link>
         </li>
       </ul>
     </div>
