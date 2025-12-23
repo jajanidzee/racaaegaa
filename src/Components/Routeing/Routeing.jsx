efec8aa639abc63e98c7d7c38a01d2d430bc3187
jajanidzee <jajanidzekote310@gmail.com>
2025-12-23 12:27:27 +0400

    Update Routeing.jsx

--- src/Components/Routeing/Routeing.jsx
+++ src/Components/Routeing/Routeing.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 function Routeing() {
   return (
-    <div className="w-[100%] justify-between flex">
+    <div className="w-full justify-between flex">
       <h1 className="Rose text-[150px] font-[East_Sea_Dokdo] hover:text-[200px] hover:text-red-500 ">
         <Link to="/"> Rose </Link>
       </h1>
 
-      <ul className="text-[34px] gap-[4px] flex">
+      <ul className="text-[34px] gap-1 flex">
         <li className="inline   ">
           <Link
             to="/About"
